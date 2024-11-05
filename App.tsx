5f9d42278c372ac1dd1ecb65ba33f6a9a664bb6d
Kalyan Basso <kalyan.basso@acqio.com.br>
2024-11-05 10:00:37 -0300

    feat: implement location tracking and map display

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
+import { useEffect, useRef, useState } from "react";
+
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+} from "expo-location";
+
+import { styles } from "./styles";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  const [currentPosition, setCurrentPosition] = useState<null | LocationObject>(
+    null
+  );
+
+  const mapRef = useRef<MapView>(null);
+
+  async function requestPermissions() {
+    const { granted } = await requestForegroundPermissionsAsync();
+    if (granted) {
+      const newCurrentPosition = await getCurrentPositionAsync();
+      setCurrentPosition(newCurrentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestPermissions();
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync(
+      {
+        accuracy: 6,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      },
+      (newPosition) => {
+        setCurrentPosition(newPosition);
+        mapRef.current?.animateCamera({
+          center: newPosition.coords,
+          pitch: 70,
+        });
+      }
+    );
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {currentPosition && (
+        <MapView
+          ref={mapRef}
+          style={styles.map}
+          initialRegion={{
+            latitude: currentPosition.coords.latitude,
+            longitude: currentPosition.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: currentPosition.coords.latitude,
+              longitude: currentPosition.coords.longitude,
+            }}
+            title="You are here"
+            description="This is your current location"
+          />
+        </MapView>
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
